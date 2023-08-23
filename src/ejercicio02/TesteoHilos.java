74b0cc1c8c931e65358422a42b733366ba4affbe
Cristian Gavilan <cristian.gavilan@est.fi.uncoma.edu.ar>
2023-08-23 19:35:24 -0300

    update

---
+++ src/ejercicio02/TesteoHilos.java
@@ -0,0 +1,9 @@
+package ejercicio02;
+
+public class TesteoHilos {
+    public static void main(String[] args) {
+        Thread miHilo = new MiEjecucion();
+        miHilo.start();
+        System.out.println("En el main");
+    }
+}
